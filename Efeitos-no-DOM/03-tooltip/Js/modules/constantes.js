1b2d902c98ef6420b0c02977333466987ff1956b
joaomagno1 <joaomagno2006@gmail.com>
2025-05-06 22:10:08 -0300

    aula06/05

---
+++ Efeitos-no-DOM/03-tooltip/Js/modules/constantes.js
@@ -0,0 +1,4 @@
+export const ano = 2025;
+export const objeto = {nome, endereco};
+export const SERVIDOR = "localhost:8080/rest";
+export const NOME_EMPRESA = "IFSP";
---
+++ Efeitos-no-DOM/03-tooltip/Js/modules/tooltip.js
@@ -0,0 +1,4 @@
+export default function initTooltip(){
+  const tooltip = document.querySelectorAll('[data-tooltip]');
+  
+}
